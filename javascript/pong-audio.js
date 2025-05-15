39575fe7ecd6634c671c42c5eeedc142c6ead9b4
kjj-johns <katherinejohnson7@my.unt.edu>
2025-05-15 13:08:37 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -49,7 +49,7 @@ soundArray.push(wallSound); //add sound to list of sounds
 export var paddleSound = new soundFile("silence.mp3");
 soundArray.push(paddleSound);
 
-export var scoreSound = new soundFile("silence.mp3");
+export var scoreSound = new soundFile("FireFlyby.mp3");
 soundArray.push(scoreSound);
 
 export var ambientSound = new soundFile("fireplace.mp3");
